a7247777928ee9d39b0e41234b1728a6ae6da580
Jugal Kishor Sahu Doctors EHR <jugalksahudocsehr@gmail.com>
2025-03-28 13:07:12 +0530

    file upload

--- src/main/java/com/example/upload/FileUploadController.java
+++ src/main/java/com/example/upload/FileUploadController.java
@@ -1,20 +1,28 @@
 package com.example.upload;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/upload")
 @CrossOrigin
 public class FileUploadController {
-    
+
     @PostMapping(consumes = "multipart/form-data")
-    public String uploadFile(
+    public ResponseEntity uploadFile(
             @RequestPart("file") MultipartFile file,
             @RequestPart("request") UploadRequest request) {
-        
-        return "File: " + file.getOriginalFilename() + " uploaded successfully!";
+
+        HashMap resp = new HashMap() {{
+            put("file", file.getOriginalFilename());
+            put("request", request);
+        }};
+        return ResponseEntity.ok().body(resp);
     }
 }
